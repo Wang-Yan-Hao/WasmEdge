f545228d342d58c6dfe18f0e46e15d6de3d45362
Yan-Hao-Wang <bses30074@gmail.com>
2026-03-18 12:52:05 +0800

    feat: add dependency tree structure and APIs
    Signed-off-by: Yan-Hao-Wang <bses30074@gmail.com>

--- include/runtime/storemgr.h
+++ include/runtime/storemgr.h
@@ -16,6 +16,7 @@
 #include "runtime/instance/component/component.h"
 #include "runtime/instance/module.h"
 
+#include <map>
 #include <mutex>
 #include <shared_mutex>
 #include <vector>
@@ -75,7 +76,12 @@ public:
       (const_cast<Instance::ModuleInstance *>(ModInst))
           ->unlinkStore(this, Name);
     }
+    for (auto &&[ModInst, Node] : DependencyTree) {
+      (const_cast<Instance::ModuleInstance *>(ModInst))
+          ->unlinkStore(this, ModInst->getModuleName());
+    }
     NamedMod.clear();
+    DependencyTree.clear();
   }
 
   /// Register named module into this store.
@@ -92,17 +98,38 @@ public:
       return Unexpect(ErrCode::Value::ModuleNameConflict);
     }
     NamedMod.emplace(std::string(Name), ModInst);
+    DependencyTree[ModInst];
     // Link the module instance to this store manager.
     (const_cast<Instance::ModuleInstance *>(ModInst))
         ->linkStore(this, Name,
                     [](const Instance::ModuleInstance::LinkedStoreKey &Key,
-                       const Instance::ModuleInstance *) {
+                       const Instance::ModuleInstance *Inst) {
                       // The unlink callback: erase the alias name from the
                       // store.
                       std::unique_lock CallbackLock(Key.first->Mutex);
                       (Key.first->NamedMod).erase(Key.second);
+                      auto It = (Key.first)->DependencyTree.find(Inst);
+                      if (It != (Key.first)->DependencyTree.end()) {
+                        (Key.first)->unsafeUnlinkDependency(It);
+                      }
                     });
     return {};
+  }
+
+  void linkAnonymousModule(const Instance::ModuleInstance *ModInst) {
+    std::unique_lock Lock(Mutex);
+    DependencyTree[ModInst];
+
+    (const_cast<Instance::ModuleInstance *>(ModInst))
+        ->linkStore(this, ModInst->getModuleName(),
+                    [](const Instance::ModuleInstance::LinkedStoreKey &Key,
+                       const Instance::ModuleInstance *Inst) {
+                      std::unique_lock CallbackLock(Key.first->Mutex);
+                      auto It = (Key.first)->DependencyTree.find(Inst);
+                      if (It != (Key.first)->DependencyTree.end()) {
+                        (Key.first)->unsafeUnlinkDependency(It);
+                      }
+                    });
   }
 
   /// Unregister a named module from this store.
@@ -114,6 +141,15 @@ public:
     }
     (const_cast<Instance::ModuleInstance *>(Iter->second))
         ->unlinkStore(this, Name);
+    auto TreeIter = DependencyTree.find(
+        (const_cast<Instance::ModuleInstance *>(Iter->second)));
+    if (TreeIter != DependencyTree.end()) {
+      TreeIter->second.IsRegistered = false;
+      if (TreeIter->second.InDegree == 0) {
+        unlinkDependency(const_cast<Instance::ModuleInstance *>(Iter->second));
+      }
+    }
+
     NamedMod.erase(Iter);
     return {};
   }
@@ -127,6 +163,23 @@ public:
     }
     NamedComp.emplace(CompInst->getComponentName(), CompInst);
     return {};
+  }
+
+  bool unlinkDependency(const Instance::ModuleInstance *Inst) {
+    std::unique_lock Lock(Mutex);
+
+    auto It = DependencyTree.find(Inst);
+    if (It == DependencyTree.end()) {
+      return true;
+    }
+
+    It->second.IsRegistered = false;
+    if (It->second.InDegree == 0) {
+      unsafeUnlinkDependency(It);
+      return true;
+    }
+
+    return false;
   }
 
 private:
@@ -152,6 +205,60 @@ private:
   /// instance here to keep the instances.
   /// FIXME: Is this necessary to be a vector?
   std::unique_ptr<Instance::ModuleInstance> FailedMod;
+
+  /// \brief Node structure representing a module's dependency state.
+  struct ModuleDependency {
+    uint32_t InDegree = 0;
+    bool IsRegistered = true;
+    std::vector<const Instance::ModuleInstance *> OutDegree;
+  };
+
+  /// \name Module dependency tree
+  std::map<const Instance::ModuleInstance *, ModuleDependency> DependencyTree;
+
+  /// \brief Registers a module instance and its dependency
+  /// \param ModInst The module instance being registered.
+  /// \param DetectedDeps The set of module instances that ModInst depends on.
+  void
+  addDependency(const Instance::ModuleInstance *Consumer,
+                const std::set<const Instance::ModuleInstance *> &Providers) {
+    if (!Consumer)
+      return;
+
+    std::unique_lock Lock(Mutex);
+
+    auto &ConsumerNode = DependencyTree[Consumer];
+
+    for (const auto *Provider : Providers) {
+      if (!Provider)
+        continue;
+      ConsumerNode.OutDegree.push_back(Provider);
+      DependencyTree[Provider].InDegree++;
+    }
+  }
+
+  void unsafeUnlinkDependency(std::map<const Instance::ModuleInstance *,
+                                       ModuleDependency>::iterator It) {
+    std::vector<const Instance::ModuleInstance *> Providers =
+        std::move(It->second.OutDegree);
+
+    DependencyTree.erase(It);
+
+    for (auto *Provider : Providers) {
+      auto ProvIt = DependencyTree.find(Provider);
+      if (ProvIt != DependencyTree.end()) {
+        if (ProvIt->second.InDegree > 0) {
+          ProvIt->second.InDegree--;
+        }
+
+        if (!ProvIt->second.IsRegistered && ProvIt->second.InDegree == 0) {
+          const auto *OrphanInst = ProvIt->first;
+          unsafeUnlinkDependency(ProvIt);
+          delete OrphanInst;
+        }
+      }
+    }
+  }
 };
 
 } // namespace Runtime
